374cedf945a8a33c60a3de2ddaf2acaa39f0e14f
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2024-07-18 02:04:39 +0800

    add react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-import logo from "./logo.svg";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import "./App.css";
+import ListUser from "./componets/ListUser";
+import CreateUser from "./componets/CreateUser";
+import EditUser from "./componets/EditUser";
 
 function App() {
   return (
     <div className="App">
-      <h1>WELCOME REACT</h1>
+      <h1>React CRUD operation using PHP API and MySQL</h1>
+      <BrowserRouter>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">List User</Link>
+            </li>
+            <li>
+              <Link to="user/create">Create User</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route index element={<ListUser />} />
+          <Route path="user/create" element={<CreateUser />} />
+          <Route path="user/:id/edit" element={<EditUser />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
