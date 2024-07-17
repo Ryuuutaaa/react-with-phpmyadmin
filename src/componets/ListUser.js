c7cfef9a405592a54b244e966773b90bdd20927e
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2024-07-18 02:54:51 +0800

    add tabe and form listUser

--- src/componets/ListUser.js
+++ src/componets/ListUser.js
@@ -4,6 +4,41 @@ function ListUser() {
   return (
     <div>
       <h1>List User</h1>
+      <form action="">
+        <table cellSpacing="10">
+          <tbody>
+            <tr>
+              <th>
+                <label htmlFor="">Name</label>
+              </th>
+              <td>
+                <input type="text" name="name" />
+              </td>
+            </tr>
+            <tr>
+              <th>
+                <label htmlFor="">Email</label>
+              </th>
+              <td>
+                <input type="text" name="email" />
+              </td>
+            </tr>
+            <tr>
+              <th>
+                <label htmlFor="">Mobile</label>
+              </th>
+              <td>
+                <input type="text" name="mobile" />
+              </td>
+            </tr>
+            <tr>
+              <td colSpan="2" align="right">
+                <button>Save</button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </form>
     </div>
   );
 }
